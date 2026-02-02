abe011869c0200c59289451d7a66b2ee543be122
Brian Tripp <btripp365@gmail.com>
2026-02-02 17:50:51 -0600

    Revert "add tiny Java Web Server (SparkJava) so that replit will allow me to publish"
    This reverts commit 370e5111aa5351efaca6aaeedb37b650c2d03058.

--- README.md
+++ README.md
@@ -7,10 +7,6 @@ Haunted House is a text based adventure game in which the user can explore rooms
 
 ## Play Haunted House
 
-Click the link below to run the program in GitHub Codespaces:
-
-[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/bdtripp/haunted_house)
-
 Click the link below to run the program on repl.it:
 
 [![Run on Replit](https://replit.com/badge/github/bdtripp/haunted_house)](https://replit.com/@BrianTripp/Haunted-House)
--- src/com/bdtripp/hauntedhouse/HauntedHouse.java
+++ src/com/bdtripp/hauntedhouse/HauntedHouse.java
@@ -1,6 +1,5 @@
 package com.bdtripp.hauntedhouse;
 
-import static spark.Spark.*;
 /**
  * The main class of the Haunted House game. This class contains the first method that is
  * executed when the game is run.
@@ -12,10 +11,6 @@ public class HauntedHouse
 {
     public static void main(String[] args)
     {
-        // This satisfies Replit's Health Check
-        port(8080);
-        get("/", (req, res) -> "Haunted House Engine Online");
-
         Game game = new Game();
         game.play();
     }
