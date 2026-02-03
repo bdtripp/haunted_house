dd05ceca2cb0c8196d6f8bbcbacf4f151b688ee6
Brian Tripp <btripp365@gmail.com>
2026-02-03 12:42:18 -0600

    add Spring Boot app to main class

--- src/main/java/com/bdtripp/hauntedhouse/HauntedHouse.java
+++ src/main/java/com/bdtripp/hauntedhouse/HauntedHouse.java
@@ -1,5 +1,8 @@
 package com.bdtripp.hauntedhouse;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * The main class of the Haunted House game. This class contains the first method that is
  * executed when the game is run.
@@ -7,10 +10,14 @@ package com.bdtripp.hauntedhouse;
  * @author Brian Tripp
  * @version 2020.06.13
  */
+
+@SpringBootApplication
 public class HauntedHouse
 {
     public static void main(String[] args)
     {
+        SpringApplication.run(HauntedHouse.class, args);
+
         Game game = new Game();
         game.play();
     }
