35c0a813d248cb8fdc7b7238a2c92f4b0a853504
Brian Tripp <btripp365@gmail.com>
2026-02-06 18:11:12 -0600

    create html, js, and css files and get Spring Boot set up to set up a Hello World page

--- src/main/java/com/bdtripp/hauntedhouse/WebApp.java
+++ src/main/java/com/bdtripp/hauntedhouse/WebApp.java
@@ -16,7 +16,7 @@ public class WebApp
 {
     public static void main(String[] args)
     {
-        SpringApplication.run(HauntedHouse.class, args);
+        SpringApplication.run(WebApp.class, args);
 
     }
 }
