4ca82b99f4dd78ff226e3fc262c90ba7c26ff0f0
Brian Tripp <btripp365@gmail.com>
2026-02-10 16:09:07 -0600

    improve Javadoc for CommandWord and Command and begin to refactor CommandWord from a class to an enum

---
+++ src/main/java/com/bdtripp/hauntedhouse/CommandWord.java
@@ -0,0 +1,45 @@
+package com.bdtripp.hauntedhouse;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * An enumeration of all command words known to the game.
+ *
+ * @author  Michael Kölling, David J. Barnes, and Brian Tripp
+ * @version 2026.02.10
+ */
+
+public enum CommandWord
+{
+    GO, QUIT, HELP, LOOK, EAT, BACK, TAKE, DROP,
+    ITEMS, STATS, CHARGE, FIRE, TALK, GIVE;
+
+    /**
+     * Checks whether a given string is a valid command word.
+     * @param aString The string to check
+     * @return true if a given string is a valid command,
+     * false if it isn't.
+     */
+    public static boolean isCommand(String aString)
+    {
+        try {
+            CommandWord.valueOf(aString.toUpperCase());
+            return true;
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    /**
+     * Get a list of all valid commands
+     * @return A list of all valid commands separated by spaces
+     */
+    public static String getCommandList() 
+    {
+        return Arrays.stream(CommandWord.values())
+                     .map(Enum::name)
+                     .map(String::toLowerCase)
+                     .collect(Collectors.joining(" "));
+    }
+}
--- src/main/java/com/bdtripp/hauntedhouse/Parser.java
+++ src/main/java/com/bdtripp/hauntedhouse/Parser.java
@@ -1,6 +1,9 @@
 package com.bdtripp.hauntedhouse;
 
 import java.util.Scanner;
+
+import com.bdtripp.hauntedhouse.CommandWords.CommandWord;
+
 import java.io.InputStream;
 
 /**
@@ -20,12 +23,10 @@ import java.io.InputStream;
  */
 public class Parser
 {
-    private CommandWords commands;  // holds all valid command words
     private Scanner reader;         // source of command input
 
     private Parser(Scanner reader) // shared constructor for both CLI and web
     {
-        commands = new CommandWords();
         this.reader = reader;
     }
 
@@ -62,9 +63,9 @@ public class Parser
             }
         }
 
-        // Now check whether this word is known. If so, create a command
+        // Now check whether this word is a valid command. If so, create a command
         // with it. If not, create a "null" command (for unknown command).
-        if(commands.isCommand(word1)) {
+        if(CommandWord.isCommand(word1)) {
             return new Command(word1, word2, word3);
         }
         else {
